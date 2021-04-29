175d732262a039e3a9a21dd26ae925daa2fbbe79
Shawne Zhang <37293252+martyrzsd@users.noreply.github.com>
2021-04-29 22:15:17 +0800

    reformat

--- code for linear model.R
+++ code for linear model.R
@@ -19,16 +19,17 @@ GLHT <-
     # MLE computation of the residue
     y <- response_input - t(B1 %*% t(independent_input[, block]))
     B2_estimate <-
-      crossprod(y, independent_input[, -block]) %*% solve(crossprod(independent_input[, -block], independent_input[, -block]))
+      crossprod(y, independent_input[,-block]) %*% solve(crossprod(independent_input[,-block], independent_input[,-block]))
     Sigma1_estimate <-
-      cov(y - t(B2_estimate %*% t(independent_input[, -block])))
+      cov(y - t(B2_estimate %*% t(independent_input[,-block])))
     # log-likelihood ratio statistics
     Lambda <- det(Sigma_estimate) / det(Sigma1_estimate)
     # Different Correction
-    if (correction_methods == "BBC") {    # BBC correction enable
+    if (correction_methods == "BBC") {
+      # BBC correction enable
       k <-
         nrow(independent_input) - ncol(independent_input) - (ncol(response_input) - length(block) +
-                                                     1) / 2
+                                                               1) / 2
       U <- -k * log(Lambda)
       threshold <-
         qchisq(1 - alpha, df = ncol(response_input) * length(block))
@@ -36,8 +37,9 @@ GLHT <-
         return(1) # reject
       }
       return(0) # accept
-    } 
-    if (correction_methods == "none") { # No Correction
+    }
+    if (correction_methods == "none") {
+      # No Correction
       U <- -nrow(independent_input) * log(Lambda)
       threshold <-
         qchisq(1 - alpha, df = ncol(response_input) * length(block))
@@ -46,77 +48,79 @@ GLHT <-
       }
       return(0) # accept
     }
-    if (correction_methods == "RMT"){
-      y1 <- ncol(response_input)/length(block)
-      y2 <- ncol(response_input)/(nrow(response_input)-ncol(independent_input))
-      hn <- sqrt(y1+y2-y1*y2)
-      an <- (1-hn)^2/(1-y2)^2
-      bn <- (1+hn)^2/(1-y2)^2
-      dn <- (sqrt(1+y2*bn/y1) - sqrt(1+y2*an/y1))/2
-      cn <- (sqrt(1+y2*bn/y1) + sqrt(1+y2*an/y1))/2
-      F_f <- (y2-1)/y2 * log(cn) + (y1-1)/y1*log(cn-dn*hn)+(y1+y2)*log((cn*hn-dn*y2)/hn)/(y1*y2)
-      m <- log((cn^2-dn^2)*hn^2/(cn*hn-y2*dn)^2)/2
-      v <- 2*log(cn^2/(cn^2-dn^2))
-      CLRT <- (-log(Lambda) - ncol(response_input)*F_f -m)*v^{-1/2}
-      Upperthreshold <- qnorm(1-alpha/2)
-      Lowerthreshold <- qnorm(alpha/2)
-      if (CLRT > Upperthreshold||CLRT<Lowerthreshold) {
+    if (correction_methods == "RMT") {
+      y1 <- ncol(response_input) / length(block)
+      y2 <-
+        ncol(response_input) / (nrow(response_input) - ncol(independent_input))
+      hn <- sqrt(y1 + y2 - y1 * y2)
+      an <- (1 - hn) ^ 2 / (1 - y2) ^ 2
+      bn <- (1 + hn) ^ 2 / (1 - y2) ^ 2
+      dn <- (sqrt(1 + y2 * bn / y1) - sqrt(1 + y2 * an / y1)) / 2
+      cn <- (sqrt(1 + y2 * bn / y1) + sqrt(1 + y2 * an / y1)) / 2
+      F_f <-
+        (y2 - 1) / y2 * log(cn) + (y1 - 1) / y1 * log(cn - dn * hn) + (y1 + y2) *
+        log((cn * hn - dn * y2) / hn) / (y1 * y2)
+      m <- log((cn ^ 2 - dn ^ 2) * hn ^ 2 / (cn * hn - y2 * dn) ^ 2) / 2
+      v <- 2 * log(cn ^ 2 / (cn ^ 2 - dn ^ 2))
+      CLRT <- (-log(Lambda) - ncol(response_input) * F_f - m) * v ^ {
+        -1 / 2
+      }
+      Upperthreshold <- qnorm(1 - alpha / 2)
+      Lowerthreshold <- qnorm(alpha / 2)
+      if (CLRT > Upperthreshold || CLRT < Lowerthreshold) {
         return(1) # reject
       }
       return(0) # accept
     }
     # Result determination
-
+    
   }
 
 
 # Data Generation
 
 
-DataPreparation <- function(
-                            response_dimension ,
+DataPreparation <- function(response_dimension ,
                             RSD ,
-                            input_dimension ) {
+                            input_dimension) {
   # p: dimension of response
   # q: dimension of input
   # n: sample size
   # We are exporting all data directly inside the global environment
   # compute the rounded sample size
-  n <- ceiling(response_dimension / RSD) 
-    # Def of the real B
-    B <<-
-      mvrnorm(response_dimension,
-              rep(0, times = input_dimension),
-              diag(1, nrow = input_dimension))
-
-    # Data Generation
-    ## Design generation
-    independent_data <<-
-      mvrnorm(n,
-              rep(1, times = input_dimension),
-              diag(
-                x = rep(0.5, times = input_dimension),
-                nrow = input_dimension
-              ))
-    ## Generation of covariance matrix for noise
-    Sigma <- diag(x = 1, nrow = response_dimension)
-    rho <- 0.9
-    ### Lower diagonal
-    for (i in 1:((ncol(Sigma)) - 2)) {
-      diag(Sigma[-1:-i, (-ncol(Sigma) + i - 1):-ncol(Sigma)]) <-
-        rep(0.9 ^ (i), times = ncol(Sigma) - i)
-    }
-    ### Corner element
-    Sigma[ncol(Sigma), 1] <- rho ^ (ncol(Sigma))
-    ### Upper diagonal
-    Sigma <-
-      Sigma + t(Sigma) - diag(diag(Sigma)) 
-    ## Generate Gaussian noise with respect to the covariance matrix
-    noise <-
-      mvrnorm(n, rep(1, times = response_dimension), Sigma) 
-    # Generate response with respect to the real model
-    response_data <<-
-      t(B %*% t(independent_data)) + noise # with rows being samples
+  n <- ceiling(response_dimension / RSD)
+  # Def of the real B
+  B <<-
+    mvrnorm(response_dimension,
+            rep(0, times = input_dimension),
+            diag(1, nrow = input_dimension))
+  
+  # Data Generation
+  ## Design generation
+  independent_data <<-
+    mvrnorm(n,
+            rep(1, times = input_dimension),
+            diag(x = rep(0.5, times = input_dimension),
+                 nrow = input_dimension))
+  ## Generation of covariance matrix for noise
+  Sigma <- diag(x = 1, nrow = response_dimension)
+  rho <- 0.9
+  ### Lower diagonal
+  for (i in 1:((ncol(Sigma)) - 2)) {
+    diag(Sigma[-1:-i, (-ncol(Sigma) + i - 1):-ncol(Sigma)]) <-
+      rep(0.9 ^ (i), times = ncol(Sigma) - i)
+  }
+  ### Corner element
+  Sigma[ncol(Sigma), 1] <- rho ^ (ncol(Sigma))
+  ### Upper diagonal
+  Sigma <-
+    Sigma + t(Sigma) - diag(diag(Sigma))
+  ## Generate Gaussian noise with respect to the covariance matrix
+  noise <-
+    mvrnorm(n, rep(1, times = response_dimension), Sigma)
+  # Generate response with respect to the real model
+  response_data <<-
+    t(B %*% t(independent_data)) + noise # with rows being samples
   # return()
 }
 
@@ -131,11 +135,11 @@ testTimes = 100
 
 # Running LLR test for all three corrections for size
 
-for (correction_methods in c("none","BBC","RMT")) {
+for (correction_methods in c("none", "BBC", "RMT")) {
   results_for_one_method <- c() # Initialize array for results
   for (k in 1:testTimes) {
     # Data preparation
-    DataPreparation(p,RSD,input_dimension)
+    DataPreparation(p, RSD, input_dimension)
     # For convenience, we choose the following null
     B1 <- B[, block]
     results_for_one_method[length(results_for_one_method) + 1] <-
